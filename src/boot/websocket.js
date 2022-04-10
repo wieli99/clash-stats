fcd4c92f64e8c93aa54202380bc46f218c13e112
Florian Wieland <99wieli@gmail.com>
2022-04-10 22:18:54 +0200

    change server to https & domain

--- src/boot/websocket.js
+++ src/boot/websocket.js
@@ -2,7 +2,7 @@ import {boot} from "quasar/wrappers"
 
 const {io} = require("socket.io-client")
 
-const socket = io("http://82.165.166.229:80")
+const socket = io("https://clash.wieli.at:443")
 
 export default boot(async ({app}) => {
 })
