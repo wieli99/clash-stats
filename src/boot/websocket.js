737b5f76cd8d6e0f610d812953dde8dfa50f9f63
Florian Wieland <99wieli@gmail.com>
2022-04-10 21:14:27 +0200

    change server ip

--- src/boot/websocket.js
+++ src/boot/websocket.js
@@ -2,7 +2,7 @@ import {boot} from "quasar/wrappers"
 
 const {io} = require("socket.io-client")
 
-const socket = io("http://localhost:3000")
+const socket = io("http://82.165.166.229:80")
 
 export default boot(async ({app}) => {
 })
